ed7f6639a0ae604b5c77fd736a39259f45acbf8c
Serdar Demirci <o_serdardemirci@hotmail.com>
2021-02-06 12:48:51 +0100

    Bank Simulator

---
+++ BankSimulator/CustomerManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace BankSimulator
+{
+    public class CustomerManager
+    {
+        public CustomerManager()
+        {
+        }
+
+        public void Add(Customer customer)
+        {
+            Console.WriteLine("A new customer added!");
+        }
+
+        public void Show(Customer[] customers)
+        {
+            foreach (Customer customer in customers)
+            {
+                Console.WriteLine(customer.Name);
+            }
+        }
+
+        public void Remove(Customer customer)
+        {
+            Console.WriteLine(customer.Id + " " + customer.Name + " removed");
+        }
+    }
+}
---
+++ BankSimulator/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace BankSimulator
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Customer customer1 = new Customer
+            {
+                Id = 1,
+                Name = "Serdar",
+                Address = "Pattern Street 7"
+            };
+
+            Customer customer2 = new Customer
+            {
+                Id = 2,
+                Name = "Ali",
+                Address = "Design Street 7"
+            };
+
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add(customer1);
+            customerManager.Add(customer2);
+
+            Customer[] customers = new Customer[] { customer1, customer2 };
+            customerManager.Show(customers);
+
+            customerManager.Remove(customer2);
+        }
+    }
+}
