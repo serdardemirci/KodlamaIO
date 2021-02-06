140bff93596f51276cb69e891eea37bbb0f43842
Serdar Demirci <o_serdardemirci@hotmail.com>
2021-02-06 12:54:42 +0100

    File added - Customer.cs

---
+++ BankSimulator/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace BankSimulator
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string IBAN { get; set; }
+        public string BIC { get; set; }
+
+        public Customer()
+        {
+        }
+    }
+}
