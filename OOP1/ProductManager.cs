3b9a28b7192760f5fba7131bcfe20e5e8e8912b5
Serdar Demirci <o_serdardemirci@hotmail.com>
2021-02-13 01:35:45 +0100

    test

---
+++ OOP1/ProductManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace OOP1
+{
+    public class ProductManager
+    {
+        public ProductManager()
+        {
+        }
+
+        public void Add(Product product)
+        {
+            Console.WriteLine(product.ProductName + " added.");
+        }
+    }
+}
